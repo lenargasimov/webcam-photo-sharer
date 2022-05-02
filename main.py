8e28749c6e724339907ae092f8e937291478ff74
lenargasimov <lenargasimov@duck.com>
2022-05-02 14:02:55 +0300

    feat: added implementing the search image functionality

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+import wikipedia
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
@@ -8,7 +11,18 @@ Builder.load_file('frontend.kv')
 class FirstScreen(Screen):
 
     def search_image(self):
-        self.manager.current_screen.ids.img.source = 'files/sunny.jpg'
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and the first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        # Download image
+        req = requests.get(image_link)
+        image_path = 'files/image.jpg'
+        with open(image_path, 'wb') as file:
+            file.write(req.content)
+        # Set the image in the Image widget
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
