32db6863cb42b11c547887b0099e0a9154393348
lenargasimov <lenargasimov@duck.com>
2022-05-10 20:26:10 +0300

    refactor: created the empty classes

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
-import requests
-import wikipedia
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+from filesharer import FileSharer
+
 Builder.load_file('frontend.kv')
 
 
-class FirstScreen(Screen):
+class CameraScreen(Screen):
+    def start(self):
+        pass
 
-    def get_image_link(self):
-        # Get user query from TextInput
-        query = self.manager.current_screen.ids.user_query.text
-        # Get wikipedia page and the first image link
-        page = wikipedia.page(query)
-        image_link = page.images[0]
-        return image_link
+    def stop(self):
+        pass
 
-    def download_image(self):
-        # Download image
-        req = requests.get(self.get_image_link())
-        image_path = 'files/image.jpg'
-        with open(image_path, 'wb') as file:
-            file.write(req.content)
-        # Set the image in the Image widget
-        return image_path
+    def capture(self):
+        pass
 
-    def set_image(self):
-        self.manager.current_screen.ids.img.source = self.download_image()
+
+class ImageScreen(Screen):
+    pass
 
 
 class RootWidget(ScreenManager):
